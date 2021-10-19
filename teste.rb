d5029c167a2aa0fe39a7df8b2ddcdeae3cbe9673
Guilherme Viana <gaviana@rd.com.br>
2021-10-18 23:57:25 -0300

    Ruby commit

---
+++ teste.rb
@@ -0,0 +1,26 @@
+
+# escreva um programa que calcula e exiba na tela: 
+
+# Quantas horas há em um ano?
+
+puts "Em um ano há ", (24 * 365), "horas"
+
+#Quantos minutos há em uma década? 
+
+puts "Em um dia Há ",  (60 * 24), "minutos"
+puts "Em um ano Há",   (1440 * 365), "minutos"
+puts "Em uma década Há", (525600 * 120), "Minutos em média"
+
+#Qual sua idade em segundos? 
+
+puts "Minha idade em segundos é ", (315360000 * 432)
+
+#Quantos chocolates você pretente come na vida? vou supor que viverei até os 80 anos.
+
+puts "Eu irei comer", (120 * 960), "Chocolates até o final da minha vida!!!"
+
+#Se minha idade é 1232 milhões de segundos, qual é minha idade em anos?
+
+puts "Minha idade é de 1232 milhões de segundos, qual é minha idade em anos é", (8640 * 3600)
+
+
